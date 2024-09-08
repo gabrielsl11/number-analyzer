929856b704f7def025aee8506643322c775bbe96
Gabriel Souza <92094301+gabrielsl11@users.noreply.github.com>
2024-09-08 17:00:02 -0300

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,14 +14,18 @@ document.getElementById('add').addEventListener('click', () => {
     }
 
     if (isnumber(number) && !inlist(number, values)) {
+        result.classList.add('text-secondary');
+        result.classList.remove('text-danger');
         values.push(number);
         let option = document.createElement('option');
-        option.text = `Value ${number} added.`;
+        option.text = `Number ${number} added.`;
         option.value = number;
         result.innerHTML = '';
         list.appendChild(option);
     } else {
-        window.alert('Invalid value or already in list.');
+        result.innerHTML = 'Invalid number or already in list.';
+        result.classList.remove('text-secondary');
+        result.classList.add('text-danger');
     }
 
     document.getElementById('number').value = '';
@@ -33,8 +37,26 @@ document.getElementById('finish').addEventListener('click', () => {
     let result = document.getElementById('result');
 
     if (values.length == 0) {
-        window.alert('add values before finishing.');
+        result.classList.remove('text-secondary');
+        result.classList.add('text-danger');
+        result.innerHTML = 'Add some numbers before finishing.';
     } else {
+        let add = document.getElementById('add');
+        let finish = document.getElementById('finish');
+        finish.disabled = true;
+        number.disabled = true;
+        
+        add.classList.remove('btn-primary');
+        add.classList.add('btn-danger');
+        add.innerHTML = 'Reload';
+
+        add.addEventListener('click', () => {
+            window.location.reload();
+        })
+
+        result.classList.add('text-secondary');
+        result.classList.remove('text-danger');
+
         let total = values.length;
         let smaller = values[0];
         let bigger = values[0];
@@ -53,10 +75,10 @@ document.getElementById('finish').addEventListener('click', () => {
 
         let average = sum / total;
 
-        result.innerHTML = `<p>There is a total of ${total} registered numbers.</p>`;
-        result.innerHTML += `<p>The smaller number is ${smaller}.</p>`;
-        result.innerHTML += `<p>The bigger number is ${bigger}.</p>`;
-        result.innerHTML += `<p>The sum of all numbers is ${sum}.</p>`;
-        result.innerHTML += `<p>The average of all numbers is ${average}.</p>`;
+        result.innerHTML = `<p>There is a total of ${total} registered numbers</p>`;
+        result.innerHTML += `<p>The smaller number is ${smaller}</p>`;
+        result.innerHTML += `<p>The bigger number is ${bigger}</p>`;
+        result.innerHTML += `<p>The sum of all numbers is ${sum}</p>`;
+        result.innerHTML += `<p>The average of all numbers is ${average}</p>`;
     }
 })
